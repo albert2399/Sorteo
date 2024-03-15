6b46ae5372cc3ed54b7d48482eb913ce36fb588e
Adrià Milan <adria4milan@gmail.com>
2024-03-15 16:47:15 +0100

    Second commit

--- main.js
+++ main.js
@@ -1,28 +1,47 @@
+import { Participante } from "./participante.js";
 
 const participantes = []
+let idParticipante = 0
 
-function createParticipante(name) {
-    let divParticipante = document.createElement('div')
-    let nameParticipante = document.createElement('p')
-    let participation = document.createElement('div')
-    divParticipante.classList.add('participante')
-    let nameNode = document.createTextNode(name)
-    nameParticipante.appendChild(nameNode)
-    divParticipante.appendChild(nameParticipante)
-    divParticipante.appendChild(participation)
-    return divParticipante
+function createParticipante(name, idParticipante) {
+  let divParticipante = document.createElement("div");
+  let nameParticipante = document.createElement("p");
+  let participation = document.createElement("div");
+  divParticipante.classList.add("participante");
+  participation.classList.add("participacion", "active");
+  participation.setAttribute('id', `pa-${idParticipante}`)
+  let nameNode = document.createTextNode(name);
+  nameParticipante.appendChild(nameNode);
+  divParticipante.appendChild(nameParticipante);
+  divParticipante.appendChild(participation);
+  return divParticipante;
 }
 
 function renderNewParticipante(name) {
-    let table = document.getElementById('table')
-    table.appendChild(createParticipante(name))
+  let table = document.getElementById("table");
+  table.appendChild(createParticipante(name));
+}
+
+function pushParticipante(name) {
+  participantes.push(new Participante(name));
+  console.log(participantes);
 }
 
 function addParticipante() {
-    let name = document.getElementById('name').value
-    renderNewParticipante(name)
+    idParticipante++
+  let name = document.getElementById("name").value;
+  if (name != '') {
+    pushParticipante(name, idParticipante);
+    renderNewParticipante(name, idParticipante);
+  }
 }
 
-function startGame() {
+function deleteParticipante() {
 
 }
+
+let btnAddParticipante = document.getElementById("addParticipante");
+btnAddParticipante.addEventListener("click", addParticipante);
+
+let btnDeleteParticipante = document.getElementById("deleteParticipante")
+btnDeleteParticipante.addEventListener("click", deleteParticipante)
