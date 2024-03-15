49302abf6b08cba7218f507628aa46cf04a39c7c
Adrià Milan <adria4milan@gmail.com>
2024-03-15 19:23:12 +0100

    Logic modifications v1.2

--- sonido.js
+++
@@ -1,7 +0,0 @@
-function sonar (){
-    var audio = new Audio('audio/disparo.mp3');
-    audio.play();
-}
-
-let btnDeleteParticipante = document.getElementById("deleteParticipante")
-btnDeleteParticipante.addEventListener("click", sonar)
