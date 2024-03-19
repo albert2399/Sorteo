c3a756a44ff2613da9f9eb0353c098179a1d36df
Adrià Milan <adria4milan@gmail.com>
2024-03-19 15:40:04 +0100

    Adding comments to the code

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+// Importamos la clase Participante desde el módulo participante.js
 import { Participante } from "./participante.js";
 
+// Array para almacenar los participantes
 const participantes = [];
+
+// Variable para el ID del próximo participante
 let idParticipante = 0;
 
+// Función para crear un nuevo elemento de participante en el DOM
 function createParticipante(name, id) {
   let divParticipante = document.createElement("div");
   let nameParticipante = document.createElement("p");
@@ -17,20 +22,24 @@ function createParticipante(name, id) {
   return divParticipante;
 }
 
+// Función para renderizar un nuevo participante en la tabla
 function renderNewParticipante(name, id) {
   let table = document.getElementById("table");
   table.appendChild(createParticipante(name, id));
 }
 
+// Función para agregar un nuevo participante al array y mostrarlo en la tabla
 function pushParticipante(id, name) {
   participantes.push(new Participante(id, name));
   console.log(participantes);
 }
 
+// Función para verificar si un nombre de participante está disponible
 function isAvailable(name) {
   return !participantes.some((participante) => participante.nombre === name);
 }
 
+// Función para agregar un participante cuando se hace clic en el botón "Agregar"
 function addParticipante() {
   let name = document.getElementById("name").value;
   if (name.trim() && isAvailable(name)) {
@@ -45,6 +54,7 @@ function addParticipante() {
   }
 }
 
+// Función para eliminar un participante cuando se hace clic en el botón "Eliminar"
 function deleteParticipante() {
   if (participantes.length > 1) {
     let aliveParticipantes = participantes.filter(
@@ -64,12 +74,15 @@ function deleteParticipante() {
   }
 }
 
+// Agregar un listener para el botón "Agregar Participante"
 let btnAddParticipante = document.getElementById("addParticipante");
 btnAddParticipante.addEventListener("click", addParticipante);
 
+// Agregar un listener para el botón "Eliminar Participante"
 let btnDeleteParticipante = document.getElementById("deleteParticipante");
 btnDeleteParticipante.addEventListener("click", deleteParticipante);
 
+// Lista de compañeros de clase predefinidos
 const classmates = [
   { id: 1, name: "Moha", participacion: true },
   { id: 2, name: "Adrià", participacion: true },
@@ -85,6 +98,7 @@ const classmates = [
   { id: 12, name: "Aitor", participacion: true },
 ];
 
+// Agregar cada compañero de clase a la lista de participantes
 classmates.forEach((classmate) => {
   idParticipante = classmate.id;
   pushParticipante(classmate.id, classmate.name);
